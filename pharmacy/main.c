6fd9f7884cfa9b540b65cef97542e1edb822b039
yousefsassy <youssef.sassi@medtech.tn>
2022-10-07 16:29:35 +0100

    Initial Commit

---
+++ pharmacy/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pharmacy
+//
+//  Created by Youssef Sassi on 7/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
